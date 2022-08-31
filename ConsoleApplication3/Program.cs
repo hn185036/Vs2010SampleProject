fd1dc5edc44bd03732209d7f99b679a2cf329039
hn185036 <hn185036@ncr.com>
2022-08-31 17:18:57 +0800

    fsdfs

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Management.Automation;
-using System.Management.Automation.Runspaces;
 using Microsoft.VisualBasic;
 
 namespace ConsoleApplication3
